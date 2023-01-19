6bc59d6f0211bd35bb8774009fb6195b01dbcc73
Samarth Chandna <samarthchandna52@gmail.com>
2023-01-19 17:09:27 -0500

    fixed hot reload

--- src/components/ProjectsContributors/Projects/Projects.jsx
+++ src/components/ProjectsContributors/Projects/Projects.jsx
@@ -1,3 +1,15 @@
+import styles from "./Projects.module.css";
+
 export default function Projects() {
-  return <></>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <div className={styles.header}>PROJECTS</div>
+        <div className={styles.projects}>
+          <div className={styles.project}>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Layout } from "./components/blob/Layout/Layout";
 import Home from "./components/Home/Home";
@@ -21,9 +21,12 @@ function App() {
   );
 }
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root"),
+  </BrowserRouter>
 );
+
+if (module.hot) {
+  module.hot.accept();
+}
